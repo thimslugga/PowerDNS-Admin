9a6f0804641d69582611f5246d777b2efe946876
adam kaminski <thimslugga@users.noreply.github.com>
2017-02-13 14:58:19 -0500

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -2,48 +2,54 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # BASIC APP CONFIG
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'We are the world'
+LOGIN_TITLE = "PowerDNS-Admin"
 BIND_ADDRESS = '127.0.0.1'
 PORT = 9393
-LOGIN_TITLE = "PDNS"
+DEBUG = True
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'iluvpowerdns'
 
 # TIMEOUT - for large zones
 TIMEOUT = 10
 
 # LOG CONFIG
-LOG_LEVEL = 'DEBUG'
+LOG_DIR = os.path.join(basedir, 'logs')
+# For Docker, leave empty string - LOG_FILE = ''
 LOG_FILE = 'logfile.log'
-# For Docker, leave empty string
-#LOG_FILE = ''
+LOG_LEVEL = 'DEBUG'
 
 # Upload
 UPLOAD_DIR = os.path.join(basedir, 'upload')
 
 # DATABASE CONFIG
-#You'll need MySQL-python
-SQLA_DB_USER = 'powerdnsadmin'
-SQLA_DB_PASSWORD = 'powerdnsadminpassword'
-SQLA_DB_HOST = 'mysqlhostorip'
-SQLA_DB_NAME = 'powerdnsadmin'
-
-#MySQL
-SQLALCHEMY_DATABASE_URI = 'mysql://'+SQLA_DB_USER+':'\
-    +SQLA_DB_PASSWORD+'@'+SQLA_DB_HOST+'/'+SQLA_DB_NAME
-#SQLite
-#SQLALCHEMY_DATABASE_URI = 'sqlite:////path/to/your/pdns.db'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+#PostgreSQL
+SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@127.0.0.1/powerdnsadmin'
+
+#SQLite
+#SQLALCHEMY_DATABASE_URI = 'sqlite:////path/to/your/pdns.db'
+
+#You'll need MySQL-python
+#SQLA_DB_USER = 'powerdnsadmin'
+#SQLA_DB_PASSWORD = 'powerdnspassword'
+#SQLA_DB_HOST = '127.0.0.1'
+#SQLA_DB_NAME = 'powerdnsadmin'
+
+#MySQL
+#SQLALCHEMY_DATABASE_URI = 'mysql://'+SQLA_DB_USER+':'\
+#    +SQLA_DB_PASSWORD+'@'+SQLA_DB_HOST+'/'+SQLA_DB_NAME
+
 # LDAP CONFIG
-LDAP_TYPE = 'ldap'
-LDAP_URI = 'ldaps://your-ldap-server:636'
-LDAP_USERNAME = 'cn=dnsuser,ou=users,ou=services,dc=duykhanh,dc=me'
-LDAP_PASSWORD = 'dnsuser'
-LDAP_SEARCH_BASE = 'ou=System Admins,ou=People,dc=duykhanh,dc=me'
+#LDAP_TYPE = 'ldap'
+#LDAP_URI = 'ldaps://your-ldap-server:636'
+#LDAP_USERNAME = 'cn=dnsuser,ou=users,ou=services,dc=duykhanh,dc=me'
+#LDAP_PASSWORD = 'dnsuser'
+#LDAP_SEARCH_BASE = 'ou=System Admins,ou=People,dc=duykhanh,dc=me'
 # Additional options only if LDAP_TYPE=ldap
-LDAP_USERNAMEFIELD = 'uid'
-LDAP_FILTER = '(objectClass=inetorgperson)'
+#LDAP_USERNAMEFIELD = 'uid'
+#LDAP_FILTER = '(objectClass=inetorgperson)'
 
 ## AD CONFIG
 #LDAP_TYPE = 'ad'
@@ -70,9 +76,9 @@ BASIC_ENABLED = True
 SIGNUP_ENABLED = True
 
 # POWERDNS CONFIG
-PDNS_STATS_URL = 'http://172.16.214.131:8081/'
-PDNS_API_KEY = 'you never know'
-PDNS_VERSION = '3.4.7'
+PDNS_STATS_URL = 'http://127.0.0.1:8081/'
+PDNS_API_KEY = 'changeme'
+PDNS_VERSION = '4.0.0'
 
 # RECORDS ALLOWED TO EDIT
 RECORDS_ALLOW_EDIT = ['A', 'AAAA', 'CNAME', 'SPF', 'PTR', 'MX', 'TXT']
