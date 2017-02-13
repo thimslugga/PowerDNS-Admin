e816e2adc20043aaa935cddb14cd52b31ae86656
adam kaminski <thimslugga@users.noreply.github.com>
2017-02-13 14:38:37 -0500

    update run.py

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-from app import app
-from config import PORT
 
-try:
-	from config import BIND_ADDRESS 
-except:
-	BIND_ADDRESS = '127.0.0.1'
+from app import app
+from config import BIND_ADDRESS, PORT, DEBUG
 
 if __name__ == '__main__':
-    app.run(debug = True, host=BIND_ADDRESS, port=PORT)
+    app.run(host=BIND_ADDRESS, port=PORT, debug=DEBUG)
