2a1bdae8c6913faa0359a22d8effdc97c65bad9a
adam kaminski <thimslugga@users.noreply.github.com>
2017-05-12 11:32:25 -0400

    Update db_upgrade.py

--- db_upgrade.py
+++ db_upgrade.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-print('Current database version: ' + str(v))
+
+
+if __name__ == "__main__":
+  api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+  v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+  print('Current database version: ' + str(v))
